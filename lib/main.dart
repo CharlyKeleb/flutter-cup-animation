f15bf7ccc5ac1850b1e48aaa9db1a329c2acab15
success <chizaramsuccess9@gmail.com>
2021-02-22 11:57:59 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:cup_animation/cup.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cup Animation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+     home: Cup(),
+   
+    );
+  }
+}
